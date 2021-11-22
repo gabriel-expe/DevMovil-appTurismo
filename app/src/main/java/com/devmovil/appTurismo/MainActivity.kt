1ffe863ee48313c0a03024b928c75a84dcd565fe
LuzPulido <lepulidop@gmail.com>
2021-11-21 20:08:02 -0500

    Edición de márgenes del Recyclerview

--- app/src/main/java/com/devmovil/appTurismo/MainActivity.kt
+++ app/src/main/java/com/devmovil/appTurismo/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 }*/
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.supportActionBar?.hide()
